f163df9ecbfc7b4098e5b01631149c1554f42bde
Abraham Maleko <abrahammaleko@gmail.com>
2021-08-25 10:38:37 +0300

    update phone number in profile settings

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -21,6 +21,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
+            'phone_no' => ['required','max:13'],
             'photo' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
         ])->validateWithBag('updateProfileInformation');
 
@@ -51,6 +52,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         $user->forceFill([
             'name' => $input['name'],
             'email' => $input['email'],
+            'phone_no' => $input['phone_no'],
             'email_verified_at' => null,
         ])->save();
 
