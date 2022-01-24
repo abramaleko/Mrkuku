f6d788129b83e6c87a340864679ea367f8bc4de5
Abraham Maleko <abrahammaleko@gmail.com>
2022-01-24 11:15:10 +0300

    removed  invoice breakdown in invoicedetails

--- app/Http/Livewire/App/Investor/InvoiceDetails.php
+++ app/Http/Livewire/App/Investor/InvoiceDetails.php
@@ -15,21 +15,6 @@ class InvoiceDetails extends Component
     public function mount($id)
     {
         $this->investment=Investments::find($id);
-
-        $this->invoiveBreakdown();
-    }
-
-    protected function invoiveBreakdown()
-    {
-       $this->investment->rear_cost=$this->investment->units * 800 ;
-
-       $this->investment->doc_quantity=( ($this->investment->units * 0.1) + $this->investment->units );
-
-       $this->investment->doc_cost=$this->investment->doc_quantity* 1500 ;
-
-       $this->investment->feed_cost=$this->investment->units* 2700 ;
-
-
     }
 
     public function render()
