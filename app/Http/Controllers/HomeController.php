a8ab33df3ff2fb1b2058fbf9f3163d3561d62283
Abraham Maleko <abrahammaleko@gmail.com>
2022-01-25 10:09:15 +0300

    removed contact page and remained support as default

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,35 +34,6 @@ class HomeController extends Controller
         return view('web.contact');
     }
 
-    public function uploadContactMessage(Request $request)
-    {
-
-         $request->validate([
-             'name' =>'required|string',
-             'email'=> 'required|email',
-             'phone_no'=> 'required|numeric',
-             'context' => 'required|string|max:500',
-             "_token" => 'required',
-         ]);
-
-         $message=new Contact();
-         $message->name=$request->name;
-         $message->email=$request->email;
-         $message->phone_no=$request->phone_no;
-         $message->context=$request->context;
-         //if user is loged in
-         if (Auth::check()) {
-            $message->user_type='Investor';
-         }
-
-         $message->save();
-
-         $request->session()->flash('success', 'Thanks for reaching out to us, our team  will contact you shortly');
-
-         return back();
-
-    }
-
     public function saveSubscriber(Request $request)
     {
         if ($request->ajax()) {
--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -4,145 +4,47 @@
 @endsection
 @section('styles')
     <link rel="stylesheet" href="{{ asset('css/contact.css') }}">
+    <style>
+
+    </style>
 @endsection
 @section('content')
     <div class="grid grid-cols-1 mx-4 mt-8 lg:mx-10 lg:gap-4 lg:grid-cols-2">
-        <div class="">
-            <h2 class="mt-4 text-xl font-bold tracking-wider text-left text-gray-600 lg:mt-8 lg:text-4xl">We'd Love to Hear From
-                you
-            </h2>
-            <p class="mt-4 text-sm font-light leading-relaxed tracking-tight text-left text-gray-500 lg:text-base">
-                Whether you are curious about anything, were ready to answer any and all questions
-            </p>
-            <div class="hidden lg:block">
-                <div class="flex mt-4 lg:mt-12">
-                    <div class="w-24 mt-4 mr-6 border-t-2 border-gray-400 lg:mt-5 lg:w-48"></div>
-                    <h2 class="text-xl font-bold tracking-wider text-center text-gray-600 lg:text-4xl">OR</h2>
-                    <div class="w-24 mt-4 ml-6 border-t-2 border-gray-400 lg:mt-5 lg:w-48"></div>
-                </div>
-                <div class="flex justify-center">
-                    <p class="mt-4 mr-4 text-sm font-light leading-relaxed tracking-tight text-gray-500 lg:mr-20 lg:text-base">
-                        Get live support from our online customer service team
-                    </p>
-                </div>
-                <div class="flex justify-center mt-6">
-                    <a href="{{route('investor.support')}}"
-                    class="py-4 mr-48 font-light tracking-wide text-white bg-blue-500 rounded shadow-2xl px-9 hover:bg-blue-300">
-                      GET SUPPORT
-                    </a>
-                </div>
-            </div>
+       <div class="">
+           <img src="{{asset('images/contact.svg')}}" alt="contact us" class="">
+       </div>
+
+       <div class="ml-4">
+        <h2 class="mt-4 text-xl font-bold tracking-wider text-left text-gray-600 lg:mt-8 lg:text-4xl">We'd Love to Hear From
+            you
+        </h2>
+        <p class="mt-4 text-sm font-light leading-relaxed tracking-tight text-left text-gray-500 lg:text-base">
+            Whether you are curious about anything, were ready to answer any and all questions
+        </p>
+        <div class="mt-8">
+            <a href="{{route('investor.support')}}"
+                class="py-4 text-base font-light text-white bg-red-500 rounded shadow-2xl lg:tracking-wide px-9 hover:bg-red-400">
+                  LEAVE MESSAGE
+                </a>
         </div>
-
-    <div class="contact-form">
-            <!-- success message -->
-            @if (Session::has('success'))
-            <div class="flex p-3 bg-green-100 rounded-md">
-                <svg class="flex-shrink-0 w-8 h-8 mr-2 text-green-600 stroke-current stroke-2" viewBox="0 0 24 24"
-                    fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M0 0h24v24H0z" stroke="none" />
-                    <circle cx="12" cy="12" r="9" />
-                    <path d="M9 12l2 2 4-4" />
-                </svg>
-
-                <div class="text-green-700">
-                    <div class="text-xl font-bold">Your message has been received!</div>
-
-                    <div>
-                        {{ Session::get('success') }}
-                    </div>
-                </div>
-            </div>
-            @endif
-
-            <form action="{{ route('uploadContactMessage') }}" autocomplete="off" id="contactus" method="POST">
-                @csrf
-                <div class="mt-8 lg:mt-12">
-                    <div class="my-8 lg:ml-8">
-                        <label for="name" class="text-lg text-gray-700">Name :</label>
-                        <input type="text" name="name" id="name" placeholder="Enter your full name"
-                            class="px-4 py-3 ml-4 text-gray-600 border-b rounded-md ">
-                            @error('name')
-                              <span class="block my-2 text-sm font-bold text-red-600">
-                                  {{$message}}
-                              </span>
-                            @enderror
-                    </div>
-                    <div class="my-8 lg:ml-8">
-                        <label for="email" class="text-lg text-gray-700">Email :</label>
-                        <input type="email" name="email" id="email" placeholder="Enter your email address"
-                            class="px-4 py-3 ml-4 text-gray-600 border-b rounded-md ">
-                            @error('email')
-                            <span class="block my-2 text-sm font-bold text-red-600">
-                                {{$message}}
-                            </span>
-                          @enderror
-                    </div>
-
-                    <div class="my-8 lg:ml-8">
-                        <label for="phone_no" class="text-lg text-gray-700">Phone no :</label>
-                        <input type="tel" name="phone_no" id="phone_no" placeholder="Enter your phone number"
-                            class="px-4 py-3 ml-2 text-gray-600 border-b rounded-md ">
-                            @error('phone_no')
-                            <span class="block my-2 text-sm font-bold text-red-600">
-                                {{$message}}
-                            </span>
-                          @enderror
-                    </div>
-
-                    <label class="block my-8 lg:ml-8">
-                        <span class="text-lg text-gray-700">Message</span>
-                        <textarea class="block px-4 mt-1 rounded-md" rows="5" name="context">
-                             </textarea>
-                             @error('context')
-                             <span class="block my-2 text-sm font-bold text-red-600">
-                                 {{$message}}
-                             </span>
-                           @enderror
-                    </label>
-
-                    <button id="recaptcha" data-sitekey="{{ config('services.recaptcha.sitekey') }}"
-                        data-callback='onSubmit' data-action='submit'
-                        class="flex justify-center px-12 py-3 mx-4 tracking-widest text-white uppercase bg-gray-800 border border-transparent rounded-md lg:py-4 lg:ml-8 g-recaptcha hover:bg-gray-600">
-                        SUBMIT
-                    </button>
-                </div>
-            </form>
-            <div class="block mt-12 lg:hidden">
-                <div class="flex mt-4">
-                    <div class="w-24 mt-4 mr-6 border-t-2 border-gray-400 lg:mt-5 lg:w-48"></div>
-                    <h2 class="text-2xl font-bold tracking-wider text-center text-gray-600 lg:text-4xl">OR</h2>
-                    <div class="w-24 mt-4 ml-6 border-t-2 border-gray-400 lg:mt-5 lg:w-48"></div>
-                </div>
-                <div class="flex justify-center">
-                    <p class="mt-4 mr-4 text-sm font-light leading-relaxed tracking-tight text-gray-500 lg:mr-20 lg:text-base">
-                        Get live support from our online customer service team
-                    </p>
-                </div>
-                <div class="flex justify-center mt-4">
-                    <a href="{{route('investor.support')}}"
-                    class="flex justify-center px-12 py-3 mx-4 tracking-widest text-white uppercase bg-blue-500 border border-transparent rounded-md shadow-xl lg:ml-8 hover:bg-blue-300">
-                      GET SUPPORT
-                    </a>
-                </div>
-            </div>
-        </div>
-
     </div>
+    </div>
+
       {{-- call us section --}}
       <div class="grid grid-cols-1 mx-10 my-16 header lg:grid-cols-2 lg:gap-20 contact">
 
         <div class="block lg:px-10">
-            <h2 class="mt-8 text-3xl font-bold text-gray-700 lg:text-4xl ">You can Call us at</h2>
+            <h2 class="mt-8 text-3xl font-bold text-gray-700 lg:text-4xl ">You Can Call Us at</h2>
             <h3 class="my-8 text-xl italic text-gray-700 lg:text-3xl number">
                 +255 659 071 309
             </h3>
-
-            <p class="text-gray-400 lg:pt-6 font-extralight" style="font-size: 22px; font-family: 'Raleway', sans-serif;">
-                Our Call center number is available from Monday to Saturday at 09:00 am to 04:00 pm, feel free to call if
+            <p class="text-lg font-light leading-relaxed tracking-tight text-left text-gray-500 lg:pt-6 lg:text-xl ">
+                Our Call center number is available from Monday to Saturday at 09:00 am to 05:00 pm, feel free to call if
                 you need
                 any assistance from us.
-            </p>
+           </p>
+
+
         </div>
 
         <div class="hidden md:block">
@@ -152,35 +54,15 @@
 
     <div class="my-16 lg:mx-10 contact">
         <div class="px-8 md:px-10">
-            <h2 class="mt-8 text-4xl font-bold text-gray-700">
-                You can Visit Us at
+            <h2 class="mt-8 text-3xl font-bold text-gray-700 lg:text-4xl">
+                You Can Visit Us at
             </h2>
             <p class="my-4 text-base font-light leading-tight tracking-tight text-left text-gray-500 lg:my-2">
                 We are open from Monday to Saturday starting from 08:30 am - 05:00 pm
             </p>
-            <h3 class="my-4 text-xl font-bold text-gray-600">Our Branches</h3>
-            <div class="flex flex-wrap">
-                <!-- Kariakoo branch -->
-                <div class="branch">
-                    <p class="text-lg text-gray-600">Dar es Salaam, Tanzania</p>
-                    <p class="text-base text-gray-600">Kariakoo, Street Livingstone & Kipata
-                        <a href="/">
-                            <img src="{{ asset('images/map.png') }}" class="w-10 h-10 my-2">
-                        </a>
-                </div>
-
-                <!-- Dar free market branch -->
-                <div class="lg:ml-40 branch">
-                    <p class="text-lg text-gray-600">Dar es Salaam, Tanzania</p>
-                    <p class="text-base text-gray-600">Dar Free Market Mall, Ali Hassan Mwinyi road
-                        <a href="/">
-                            <img src="{{ asset('images/map.png') }}" class="w-10 h-10 my-2">
-                        </a>
-                </div>
-            </div>
             <h3 class="my-4 text-xl font-bold text-gray-600">Our Headquaters</h3>
-            <!-- HQ -->
-            <div class="hq">
+             <!-- HQ -->
+             <div class="hq">
                 <p class="text-lg text-gray-600">Dar es Salaam, Tanzania</p>
                 <p class="text-base text-gray-600">Kisarawe2, Mbosai
                     <a href="https://www.google.com/maps/place/Mr+kuku+farmers+limited/@-6.9227259,39.3722372,17z/data=!3m1!4b1!4m5!3m4!1s0x185db5cda47e10e1:0xa395cf0a99209414!8m2!3d-6.9227303!4d39.3744202"
@@ -188,16 +70,31 @@
                         <img src="{{ asset('images/map.png') }}" class="w-10 h-10 my-2">
                     </a>
             </div>
+            <h3 class="my-4 text-xl font-bold text-gray-600">Our Branches</h3>
+            <div class="flex flex-wrap">
+                <!-- Dar free market branch -->
+                <div class="branch">
+                    <p class="text-lg text-gray-600">Dar es Salaam, Tanzania</p>
+                    <p class="text-base text-gray-600">Dar Free Market Mall, Ali Hassan Mwinyi road
+                        <a target="_blank"
+                        href="https://www.google.com/maps/place/Mr+kuku+farmers+limited/@-6.7846763,39.2749704,20z/data=!4m5!3m4!1s0x185c4ddec3891e2b:0xa03bb6675f49ea33!8m2!3d-6.7847019!4d39.2771171">
+                            <img src="{{ asset('images/map.png') }}" class="w-10 h-10 my-2">
+                        </a>
+                </div>
+
+                <!-- Kariakoo branch -->
+                <div class="branch lg:ml-40 ">
+                    <p class="text-lg text-gray-600">Dar es Salaam, Tanzania</p>
+                    <p class="text-base text-gray-600">Kariakoo, Street Livingstone & Kipata
+                        <a href="/">
+                            <img src="{{ asset('images/map.png') }}" class="w-10 h-10 my-2">
+                        </a>
+                </div>
+            </div>
+
         </div>
 
     </div>
 
 @endsection
-@section('scripts')
-    <script src="https://www.google.com/recaptcha/api.js"></script>
-    <script>
-        function onSubmit(token) {
-            document.getElementById("contactus").submit();
-        }
-    </script>
-@endsection
+
