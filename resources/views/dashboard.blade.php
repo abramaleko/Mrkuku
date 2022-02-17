893ec0038d6d6552172c496375d1f9029b6e9a3a
Abraham Maleko <abrahammaleko@gmail.com>
2022-02-17 11:05:25 +0300

    customized the dashboard styles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,25 +2,19 @@
     <x-slot name="title">
             {{ __('Dashboard') }}
     </x-slot>
-    <x-slot name="styles">
-       <style>
-           main
-           {
-               background-color: #ffff !important;
-           }
-       </style>
-    </x-slot>
 
-    <div class="container px-6 py-6 mx-auto">
-       <div class="flex justify-center">
-           <img src="{{asset('images/construction.jpeg')}}" alt="construction" class="w-auto">
-       </div>
-       <h1 class="px-4 py-8 text-3xl leading-normal tracking-tight text-gray-700">
-        We are currently under construction, we will soon contact you when construction is finished
-    </h1>
-    <h2 class="text-lg font-light leading-normal tracking-tight text-gray-600">
-        If you have any concerns please leave us a message <a href="{{route('contact')}}" class="text-blue-500 hover:underline"> here </a> and
-        we will contact you as soon as we can
-    </h2>
+    <div class="container flex flex-wrap justify-center px-6 py-6 mx-auto mt-24">
+<div class="">
+    <img src="{{asset('images/coming-soon.png')}}" alt="coming-soon" class="w-40 h-40 lg:h-48 lg:w-48">
+</div>
+<div class="mt-8 lg:ml-8">
+    <h1 class="text-2xl font-semibold text-gray-600 lg:text-3xl">System Will Be live Soon</h1>
+    <p class="my-4 text-lg leading-loose text-gray-800">
+        If you have any concerns please leave us a message <a href="{{route('contact')}}" class="text-blue-500 hover:underline"> here </a>
+        or call us at +255-659-071-309 and
+        we will get back to you as soon as we can
+    </p>
+
+</div>
     </div>
 </x-app-layout>
